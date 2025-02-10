0b3498a71540dbbe3c901e5bcad7f47e2cc94827
Sakhile Masoka <smasoka@oldmutual.com>
2025-02-10 19:51:01 +0200

    rename to countrydetails

---
+++ backend/app/models/countrydetails.py
@@ -0,0 +1,11 @@
+# Define data models
+
+from pydantic import BaseModel
+
+class Country(BaseModel):
+    name: str
+    flag: str
+
+class CountryDetails(BaseModel):
+    name: str
+    population: int
