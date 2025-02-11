e2f8a644fb0a81cee1ac9a0c3f3062a205342492
Sakhile Masoka <smasoka@oldmutual.com>
2025-02-11 21:40:10 +0200

    add Link to img tag

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Link from "next/link";
 
 export async function getServerSideProps() {
     const response = await axios.get("http://127.0.0.1:8000/countries");
@@ -18,7 +19,9 @@ export default function Home({ countries }) {
             <div>
                 {countries.map((country) => (
                     <div>
-                        <img src={country.flag} alt={country.name} width="100" />
+                        <Link href={`/country/${country.name}`}>
+                            <img src={country.flag} alt={country.name} width="100" style={{ cursor: "pointer"}} />
+                        </Link>
                         <p>{country.name}</p>
                     </div>
                 ))}
